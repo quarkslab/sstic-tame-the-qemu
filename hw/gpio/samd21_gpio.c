c39beb61fd9d032630489b84f3317f1626f85c51
Damien Cauquil <dcauquil@quarkslab.com>
2024-05-20 14:52:00 +0200

    First commit.

---
+++ hw/gpio/samd21_gpio.c
@@ -0,0 +1,349 @@
+/*
+ * SAMD21 general purpose input/output register definition
+ *
+ * This code is licensed under the GPL version 2 or later.  See
+ * the COPYING file in the top-level directory.
+ */
+
+#include "qemu/osdep.h"
+#include "qemu/log.h"
+#include "qemu/module.h"
+#include "hw/gpio/samd21_port.h"
+#include "hw/irq.h"
+#include "migration/vmstate.h"
+#include "trace.h"
+
+static int pull_value(uint32_t config)
+{
+    int pull = extract32(config, 2, 1);
+    if (!pull) {
+        /* Pull-up enabled. */
+        return 1;
+    } else {
+        /* high impedance if pull-up disabled. */
+        return -1;
+    }
+}
+
+static void update_output_irq(SAMD21GPIOState *s, size_t i,
+                              bool connected, bool level)
+{
+    /* If pin is not connected, reflect its floating state by setting the IRQ
+       level to -1. */
+    int64_t irq_level = connected ? level : -1;
+    bool old_connected = extract32(s->old_out_connected, i, 1);
+    bool old_level = extract32(s->old_out, i, 1);
+
+    /* If output has been disconnected/connected or level changed, notify. */
+    if ((old_connected != connected) || (old_level != level)) {
+        qemu_set_irq(s->output[i], irq_level);
+    }
+
+    /* Save current pin state. */
+    s->old_out = deposit32(s->old_out, i, 1, level);
+    s->old_out_connected = deposit32(s->old_out_connected, i, 1, connected);
+}
+
+static void update_state(SAMD21GPIOState *s)
+{
+    int pull;
+    size_t i;
+    bool connected_out, dir, connected_in, out, in, input;
+
+    for (i = 0; i < SAMD21_PORT_PINS; i++) {
+        /* Retrieve the pin pull-up configuration. */
+        pull = pull_value(s->cnf[i]);
+
+        /* Retrieve the pin direction. */
+        dir = extract32(s->cnf[i], 1, 1);
+
+        /* Determine if the pin is externally driven. */
+        connected_in = extract32(s->in_mask, i, 1);
+
+        /* Retrieve the OUT register value. */
+        out = extract32(s->out, i, 1);
+
+        /* Retrieve the IN register value. */
+        in = extract32(s->in, i, 1);
+
+        /* Determine if pin is configured as input. */
+        input = extract32(s->cnf[i], 1, 1);
+
+        /* Determine if pin is configured as output. */
+        connected_out = (dir==0);
+
+        /* If pin is configured as output and pull-up enabled, reflect pull-up
+           configuration in the IN register. */
+        if (!input) {
+            if (pull > 0) {
+                s->in = deposit32(s->in, i, 1, pull);
+            }
+        } else {
+            /* If pin is configured as input and connected to an output IRQ, then
+               we have an issue ! */
+            if (connected_out && connected_in && out != in) {
+                /* Pin both driven externally and internally */
+                qemu_log_mask(LOG_GUEST_ERROR,
+                              "GPIO pin %zu short circuited\n", i);
+            }
+
+            /* If pin is configured as input and externally driven */
+            if (connected_in) {
+                /* If pull-up is enabled and pin not connected to an ouput IRQ. */
+                if (pull > 0 && !connected_out) {
+                    /* Consider pin as connected out (temporarily). */
+                    connected_out = true;
+
+                    /* Set pin level to 1 if pull-up enabled, 0 otherwise. */
+                    out = pull;
+                }
+                if (connected_out) {
+                    /* Update IN value if pull-up enabled. */
+                    s->in = deposit32(s->in, i, 1, out);
+                }
+            }
+        }
+
+        /* Update output GPIO levels based on state. */
+        update_output_irq(s, i, connected_out, out);
+    }
+}
+
+/*
+ * Direction is exposed in both the DIR register and the DIR bit
+ * of each PINs CNF configuration register. Reflect bits for pins in DIR
+ * to individual pin configuration registers.
+ */
+static void reflect_dir_bit_in_cnf(SAMD21GPIOState *s)
+{
+    size_t i;
+
+    uint32_t value = s->dir;
+
+    for (i = 0; i < SAMD21_PORT_PINS; i++) {
+        s->cnf[i] = (s->cnf[i] & ~(2UL)) | ((~((value >> i) & 0x01)) << 1);
+    }
+}
+
+static uint64_t samd21_port_read(void *opaque, hwaddr offset, unsigned int size)
+{
+    SAMD21GPIOState *s = SAMD21_PORT(opaque);
+    size_t idx;
+    uint64_t r = 0;
+
+    switch (offset)
+    {
+        case SAMD21_PORT_REG_OUT ... SAMD21_PORT_REG_OUTCLR:
+            r = s->out;
+            break;
+
+        case SAMD21_PORT_REG_IN:
+            r = s->in;
+            break;
+
+        case SAMD21_PORT_REG_WRCONFIG:
+            r = 0;
+            break;
+
+        case SAMD21_PORT_REG_CONTROL:
+            r = 0;
+            break;
+
+        case SAMD21_PORT_REG_DIR ... SAMD21_PORT_REG_DIRCLR:
+            r = s->dir;
+            break;
+
+        case SAMD21_PORT_REG_PINCFG_START ... SAMD21_PORT_REG_PINCFG_STOP:
+            idx = (offset - SAMD21_PORT_REG_PINCFG_START) / 4;
+            r = s->cnf[idx];
+            break;
+
+        default:
+            qemu_log_mask(LOG_GUEST_ERROR,
+                    "%s: bad read offset 0x%" HWADDR_PRIx "\n",
+                        __func__, offset);
+            break;
+    }
+
+    return r;
+}
+
+static void samd21_port_write(void *opaque, hwaddr offset,
+                       uint64_t value, unsigned int size)
+{
+    SAMD21GPIOState *s = SAMD21_PORT(opaque);
+    size_t idx;
+
+    switch (offset)
+    {
+        case SAMD21_PORT_REG_OUT:
+        {
+            s->out = value;
+        }
+        break;
+
+        case SAMD21_PORT_REG_OUTSET:
+        {
+            s->out |= value;
+        }
+        break;
+
+        case SAMD21_PORT_REG_OUTCLR:
+        {
+            s->out &= ~(value);
+        }
+        break;
+
+        case SAMD21_PORT_REG_OUTTGL:
+        {
+            s->out ^= value;
+        }
+        break;
+
+        case SAMD21_PORT_REG_DIR:
+        {
+            s->dir = value;
+            reflect_dir_bit_in_cnf(s);
+        }
+        break;
+
+        case SAMD21_PORT_REG_DIRSET:
+        {
+            s->dir |= value;
+            reflect_dir_bit_in_cnf(s);
+        }
+        break;
+
+        case SAMD21_PORT_REG_DIRCLR:
+        {
+            s->dir &= ~(value);
+            reflect_dir_bit_in_cnf(s);
+        }
+        break;
+
+        case SAMD21_PORT_REG_DIRTGL:
+        {
+            s->dir ^= value;
+            reflect_dir_bit_in_cnf(s);
+        }
+        break;
+
+        case SAMD21_PORT_REG_CONTROL:
+        {
+            s->control = value;
+            qemu_log_mask(LOG_UNIMP,
+                      "%s: CONTROL register support not yet implemented !\n",
+                      __func__);
+        }
+        break;
+
+        case SAMD21_PORT_REG_WRCONFIG:
+        {
+            /* Determine which half to update. */
+             qemu_log_mask(LOG_UNIMP,
+                      "%s: WRCONFIG register support not yet implemented !\n",
+                      __func__);
+        }
+        break;
+
+        case SAMD21_PORT_REG_PMUX:
+        {
+            /* Determine which half to update. */
+             qemu_log_mask(LOG_UNIMP,
+                      "%s: PMUX register support not yet implemented !\n",
+                      __func__);
+        }
+        break;
+
+        case SAMD21_PORT_REG_PINCFG_START ... SAMD21_PORT_REG_PINCFG_STOP:
+        {
+            idx = (offset - SAMD21_PORT_REG_PINCFG_START) / 4;
+            s->cnf[idx] = value;
+            s->dir = (s->dir & ~(1UL << idx)) | ((~((value & 0x02)>>1)) << idx);
+        }
+        break;
+
+        default:
+        {
+            qemu_log_mask(LOG_GUEST_ERROR,
+                        "%s: bad write offset 0x%" HWADDR_PRIx "\n",
+                        __func__, offset);
+        }
+        break;
+    } 
+
+    update_state(s);
+}
+
+static const MemoryRegionOps gpio_ops = {
+    .read =  samd21_port_read,
+    .write = samd21_port_write,
+    .endianness = DEVICE_LITTLE_ENDIAN,
+    .impl.min_access_size = 4,
+    .impl.max_access_size = 4,
+};
+
+static void samd21_gpio_set(void *opaque, int line, int value)
+{
+    SAMD21GPIOState *s = SAMD21_PORT(opaque);
+
+    assert(line >= 0 && line < SAMD21_PORT_PINS);
+
+    s->in_mask = deposit32(s->in_mask, line, 1, value >= 0);
+    if (value >= 0) {
+        s->in = deposit32(s->in, line, 1, value != 0);
+    }
+}
+
+static void samd21_port_reset(DeviceState *dev)
+{
+    SAMD21GPIOState *s = SAMD21_PORT(dev);
+    size_t i;
+
+    s->out = 0;
+    s->in = 0;
+    s->old_out = 0;
+    s->old_out_connected = 0;
+    s->in_mask = 0;
+    s->dir = 0;
+    s->control = 0;
+
+    for (i = 0; i < SAMD21_PORT_PINS; i++) {
+        s->cnf[i] = 0x00000000;
+    }
+}
+
+static void samd21_port_init(Object *obj)
+{
+    SAMD21GPIOState *s = SAMD21_PORT(obj);
+
+    memory_region_init_io(&s->mmio, obj, &gpio_ops, s,
+            TYPE_SAMD21_PORT, SAMD21_PORT_SIZE);
+    sysbus_init_mmio(SYS_BUS_DEVICE(obj), &s->mmio);
+
+    qdev_init_gpio_in(DEVICE(s), samd21_gpio_set, SAMD21_PORT_PINS);
+    qdev_init_gpio_out(DEVICE(s), s->output, SAMD21_PORT_PINS);
+}
+
+static void samd21_port_class_init(ObjectClass *klass, void *data)
+{
+    DeviceClass *dc = DEVICE_CLASS(klass);
+
+    dc->reset = samd21_port_reset;
+    dc->desc = "SAMD21 PORT";
+}
+
+static const TypeInfo samd21_port_info = {
+    .name = TYPE_SAMD21_PORT,
+    .parent = TYPE_SYS_BUS_DEVICE,
+    .instance_size = sizeof(SAMD21GPIOState),
+    .instance_init = samd21_port_init,
+    .class_init = samd21_port_class_init
+};
+
+static void samd21_port_register_types(void)
+{
+    type_register_static(&samd21_port_info);
+}
+
+type_init(samd21_port_register_types)
---
+++ include/hw/arm/samd21_mcu.h
@@ -0,0 +1,164 @@
+/*
+ * ATMEL SAMD21 MCU
+ *
+ * Copyright 2024 Damien Cauquil <dcauquil@quarkslab.com>
+ *
+ * This code is licensed under the GPL version 2 or later.  See
+ * the COPYING file in the top-level directory.
+ */
+
+#ifndef SAMD21_MCU_H
+#define SAMD21_MCU_H
+
+#include "hw/sysbus.h"
+#include "hw/arm/armv7m.h"
+#include "hw/clock.h"
+#include "qom/object.h"
+#include "hw/arm/samd21.h"
+#include "hw/misc/samd21_sercom.h"
+#include "hw/gpio/samd21_port.h"
+
+/**
+ * The following lines define our MCU class name, "samd21-mcu",
+ * and its associated state structure `SAMD21State`.
+ * 
+ * The `OBJECT_DECLARE_SIMPLE_TYPE` macro creates a series of other
+ * macros including `SAMD21_MCU()` that will cast an opaque pointer
+ * to a pointer to a `SAMD21State` structure. This macro is intensively
+ * used in the MCU callbacks.
+ */
+
+#define TYPE_SAMD21_MCU "samd21-mcu"
+OBJECT_DECLARE_SIMPLE_TYPE(SAMD21State, SAMD21_MCU)
+
+/**
+ * We then declare the SYSCTRL registers offsets based on the datasheet.
+ * 
+ * These macros will define for each register a constant `A_<REGISTER_NAME>`
+ * and a constant `R_<REGISTER_NAME>` that contains respectively the register
+ * memory offset and the register array index (computed from register size).
+ * 
+ * For instance, the following `SYSCTRL_INTENSET` register is used with the
+ * `REG32()` macro creating a `A_SYSCTRL_INTENSET` constant of value 0x04 and
+ * a `R_SYSCTRL_INTENSET` constant of value 0x01 (= 0x04 / 4, since the register
+ * contains a 32-bit value).
+ * 
+ * In our state structure, we define a `sysctrl_regs` array that will used to
+ * hold the registers value. The `SYSCTRL_INTENSET` register index in  this
+ * array is given by `R_SYSCTRL_INTENSET`.
+ * 
+ * The `REG8()`, `REG16()` and `REG32()` macros are specifically designed for
+ * this usage.
+ * 
+ */
+
+REG32(SYSCTRL_INTENCLR, 0x00)
+REG32(SYSCTRL_INTENSET, 0x04)
+REG32(SYSCTRL_INTFLAG, 0x08)
+REG32(SYSCTRL_PCLKSR, 0x0C)
+REG32(SYSCTRL_XOSC, 0x10)
+REG32(SYSCTRL_XOSC32K, 0x14)
+REG32(SYSCTRL_OSC32K, 0x18)
+REG32(SYSCTRL_OSCULP32K, 0x1C)
+REG32(SYSCTRL_OSC8M, 0x20)
+REG32(SYSCTRL_DFLLCTRL, 0x24)
+REG32(SYSCTRL_DFLLVAL, 0x28)
+REG32(SYSCTRL_DFLLMUL, 0x2C)
+REG32(SYSCTRL_DFLLSYNC, 0x30)
+REG32(SYSCTRL_BOD33, 0x34)
+REG32(SYSCTRL_VREG, 0x3C)
+REG32(SYSCTRL_VREF, 0x40)
+REG32(SYSCTRL_DPLLCTRLA, 0x44)
+REG32(SYSCTRL_DPLLRATIO, 0x48)
+REG32(SYSCTRL_DPLLCTRLB, 0x4C)
+REG32(SYSCTRL_DPLLSTATUS, 0x50)
+
+/* Define GCLK registers offsets. */
+REG8(GCLK_CTRL, 0x00)
+REG8(GCLK_STATUS, 0x01)
+REG16(GCLK_CLKCTRL, 0x02)
+REG32(GCLK_GENCTRL, 0x04)
+REG32(GCLK_GENDIV, 0x08)
+
+/**
+ * We then define our own device state structure, `SAMD21State`.
+ * 
+ * This structure is critical as it will be used to store any instanciated
+ * object state in memory. When QEMU creates an object, it retrieves the
+ * object type (or class) information and allocate memory for the class
+ * instance it created. QEMU has no idea of the structure to use for this
+ * newly created object, but the type information provides the size required
+ * to store this structure, in bytes.
+ * 
+ * Therefore, every QEMU functions handling generic objects use *opaque*
+ * pointers that point to an instance of each object state structure. Specific
+ * callbacks that are expected this structure must convert the provided *opaque*
+ * pointer to the correct type (see `OBJECT_DECLARE_SIMPLE_TYPE` above).
+ * 
+ * This structure starts with the state structure of its parent class, in our
+ * case a System Bus Device state structure. This allows our instance structure
+ * to be used with any System Bus Device related functions, as they will use
+ * this `parent_obj` structure to read and store the parent object state.
+ * 
+ * Our object properties will be stored right after the parent object state
+ * structure, allowing us to add our own members without interfering with the
+ * parent state structure.
+ * 
+ */
+
+struct SAMD21State {
+    /*< private >*/
+    SysBusDevice parent_obj;
+
+    /*< public >*/
+
+    /* We need an ARMv7M CPU object. */
+    ARMv7MState cpu;
+
+    /* And also some memory region for our various controllers. */
+    MemoryRegion iomem;
+    MemoryRegion sram;
+    MemoryRegion flash;
+    MemoryRegion clock;
+    MemoryRegion sys;
+    MemoryRegion pm;
+    MemoryRegion aux;
+
+    /* These two members are required to store our device properties. */
+    uint32_t sram_size;         /* SRAM size. */
+    uint32_t flash_size;        /* Flash memory size */
+    MemoryRegion *board_memory; /* Board memory region we will load into our
+                                   container memory region. */
+
+    /**
+     * Hardware peripherals
+     */
+
+    /* Clock controller registers (GCLK). */
+    uint32_t gclk_regs[3];
+
+    /* SYSCTRL registers. */
+    uint32_t sysctrl_regs[0x15];
+
+    /* AUX memory space. */
+    uint32_t aux_mem[SAMD21_AUX_SIZE/4];
+
+    /* PORT controller. */
+    SAMD21GPIOState port;
+
+    /*
+     * SERCOM child object, defined and implemented
+     * in /hw/misc/samd21_sercom.c
+     */
+
+    SAMD21SERCOMState sercom[6];
+
+
+    /* Our main MCU memory container. */
+    MemoryRegion container;
+
+    /* Main CPU clock. */
+    Clock *sysclk;
+};
+
+#endif /* SAMD21_H */
